df0b93d75517e8ce039d8486e450c9b67bfc88ce
Devin <devin@cyberindo-sinergi.com>
2024-04-04 14:48:28 +0700

    Initial Commit

---
+++ walletApp_UiKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  walletApp_UiKit
+//
+//  Created by Cyberindo Sinergi Sistem on 04/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
